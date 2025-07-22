6ecc2eb3bcfc1d2aa61635e007650965e32e02b9
DinuriN <dinurin730@gmail.com>
2025-07-22 20:52:25 +0530

    minor issue fixed

--- main.py
+++ main.py
@@ -47,39 +47,39 @@ class Order:
     
     #display menu and handle user input
 
-    def main():
-        menu =[
-            Coffee("Espresso", 2.5),
-            Coffee("Latter", 3.5),
-            Coffee("Capuccino", 3.0),
-            Coffee("Americano", 2.0)
-        ]
+def main():
+    menu =[
+        Coffee("Espresso", 2.5),
+        Coffee("Latte", 3.5),
+        Coffee("Capuccino", 3.0),
+        Coffee("Americano", 2.0)
+    ]
 
-        order=Order()
+    order=Order()
 
         #user interaction
-        while True:
-            print("\n--- Coffee Menu ---")
-            for i, coffee in enumerate(menu,1):
-                print(f"{i}.{coffee.name} - ${coffee.price}")
+    while True:
+        print("\n--- Coffee Menu ---")
+        for i, coffee in enumerate(menu,1):
+            print(f"{i}.{coffee.name} - ${coffee.price}")
 
-            print("5. View Order")
-            print('6. Checkout')
-            print("7. Exit")
+        print("5. View Order")
+        print('6. Checkout')
+        print("7. Exit")
 
-            choice=input("Choose an option: ")
+        choice=input("Choose an option: ")
 
-            if choice in ['1', '2', '3', '4']:
-                order.add_item(menu[int(choice)-1])
-            elif choice == '5':
-                order.show_order()
-            elif choice == '6':
-                order.checkout()
-            elif choice == '7':
-                print("Thanks for visiting. Goodbye!")
-                break
-            else:
-                print("Invalid choice. Try again")
+        if choice in ['1', '2', '3', '4']:
+            order.add_item(menu[int(choice)-1])
+        elif choice == '5':
+            order.show_order()
+        elif choice == '6':
+            order.checkout()
+        elif choice == '7':
+            print("Thanks for visiting. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Try again")
     
-    if __name__ == "__main__":
+if __name__ == "__main__":
         main()
