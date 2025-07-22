2bd32f91f1c538399c8d62f4ced3649b66c17154
DinuriN <dinurin730@gmail.com>
2025-07-22 21:03:07 +0530

    test file added

---
+++ test_coffee.py
@@ -0,0 +1,19 @@
+import unittest
+from main import Coffee, Order
+
+class TestCoffeeOrder(unittest.TestCase):
+    def test_add_item(self):
+        order=Order()
+        coffee=Coffee("TestCoffee", 5.0)
+        order.add_item(coffee)
+        self.assertEqual(len(order.items), 1)
+        self.assertEqual(order.items[0].name, "TestCoffee")
+
+    def test_total(self):
+        order=Order()
+        order.add_item(Coffee("One", 2.0))
+        order.add_item(Coffee("Two", 3.0))
+        self.assertEqual(order.total(), 5.0)
+
+if __name__=='__main__':
+    unittest.main()
